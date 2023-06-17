db72fd66d555fde2019619d1d0aaa63115859615
f8f8f8ff <reedhdonaldson@gmail.com>
2023-06-16 21:01:49 -0700

    move handleDroppedFiles into new file

--- ui/ui.go
+++ ui/ui.go
@@ -2,17 +2,13 @@ package ui
 
 import (
 	"fmt"
-	"frame/canvas"
-	"image"
 	"image/color"
-	_ "image/jpeg"
-	_ "image/png"
-	"io/fs"
-	"log"
 	"sync"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+
+	"frame/canvas"
 )
 
 type UI struct {
@@ -53,62 +49,4 @@ func (ui *UI) Draw(screen *ebiten.Image) {
 
 	msg := fmt.Sprintf("%0.2f", ebiten.ActualFPS())
 	ebitenutil.DebugPrint(screen, msg)
-}
-
-func (ui *UI) handleDroppedFiles() error {
-	if err := func() error {
-		ui.m.Lock()
-		defer ui.m.Unlock()
-		return ui.err
-	}(); err != nil {
-		return err
-	}
-
-	if files := ebiten.DroppedFiles(); files != nil {
-		// log.Println(files)
-		go func() {
-			if err := fs.WalkDir(files, ".", func(path string, d fs.DirEntry, err error) error {
-				if err != nil {
-					return err
-				}
-
-				fi, err := d.Info()
-				if err != nil {
-					return err
-				}
-				log.Printf("Name: %s, Size: %d, IsDir: %t, ModTime: %v", fi.Name(), fi.Size(), fi.IsDir(), fi.ModTime())
-
-				f, err := files.Open(path)
-				if err != nil {
-					return err
-				}
-
-				defer func() {
-					_ = f.Close()
-				}()
-
-				if fi.IsDir() {
-					return nil
-				}
-
-				img, _, err := image.Decode(f)
-				if err != nil {
-					return err
-				}
-
-				ui.m.Lock()
-				ui.Canvas.AddImage(img)
-				ui.m.Unlock()
-
-				return nil
-			}); err != nil {
-				ui.m.Lock()
-				if ui.err == nil {
-					ui.err = err
-				}
-				ui.m.Unlock()
-			}
-		}()
-	}
-	return nil
 }
